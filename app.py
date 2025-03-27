b4c52d8b58dcf113159fc48aa6d6fee612facc03
Adam Malinovski <adammm934@gmail.com>
2025-03-27 10:49:20 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Hello from Flask son OpesnShift! triggser webhook"
+    return "Hello from Flask son OpsesnShift! triggser webhook"
 
 if __name__ == "__main__":
     # Listen on all interfaces, on port 8080
