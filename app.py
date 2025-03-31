840eeeeee74e51921d40d09c660fa530e06c2c77
adammalinovski <88232527+adammalinovski@users.noreply.github.com>
2025-03-31 21:15:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Hello from Flask son OpssesnShsissfd! stdsrser webhook"
+    return "Hello from Flask son OpssesnShsissfd! stdsrser webshook"
 
 if __name__ == "__main__":
     # Listen on all interfaces, on port 8080
