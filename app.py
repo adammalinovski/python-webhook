01c00f016a14e028afd429266e24def261b2c812
adammalinovski <88232527+adammalinovski@users.noreply.github.com>
2025-04-02 10:46:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "Hello from webhook on git push"
+    return "Hello from webhook on git pushs"
 
 if __name__ == "__main__":
     # Listen on all interfaces, on port 8080
